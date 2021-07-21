b4610bc770299fe2aff0f5e9b5709bf686d76f04
ahmadpiee <ahmadpiee69@gmail.com>
2021-07-21 15:29:44 +0700

    refactoring productRoutes and add controller for getting product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,39 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
 const router = express.Router();
+import {
+    getProductById,
+    getProducts,
+} from "../controllers/productController.js";
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-router.get(
-    "/",
-    asyncHandler(async (req, res) => {
-        try {
-            const products = await Product.find({});
-            res.json(products);
-        } catch (error) {
-            alert(error);
-        }
-    })
-);
-
-// @desc Fetch single product
-// @route GET /api/:id
-// @access Public
-
-router.get(
-    "/:id",
-    asyncHandler(async (req, res) => {
-        const product = await Product.findById(req.params.id);
-
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404);
-            throw new Error("Product not found");
-        }
-    })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
