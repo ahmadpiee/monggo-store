0c7ee12dfb70be059ebd39e05e3231ab0c0aa82f
ahmadpiee <ahmadpiee69@gmail.com>
2021-07-26 16:13:25 +0700

    added redux logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { persistedReducer } from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 const middleware = [thunk];
 
 export const store = createStore(
     persistedReducer,
-    composeWithDevTools(applyMiddleware(...middleware))
+    composeWithDevTools(applyMiddleware(...middleware, logger))
 );
 
 export const persistor = persistStore(store);
